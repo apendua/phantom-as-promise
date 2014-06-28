cdd44b980e86579c4a54bd3cf1036fdbd5a82e9f
apendua <apendua@gmail.com>
2014-06-29 00:42:33 +0200

    Added some basic features

--- phantom-as-promise.js
+++ phantom-as-promise.js
@@ -1,5 +1,51 @@
+var Phantom = require('node-phantom-simple');
+var Promise = require('es6-promise').Promise;
 
 module.exports = {
 
+  Phantom: function (options) {
+    return new Promise(function (resolve, reject) {
+      phantom.create(function (err, phantom) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(wrap(phantom));
+        }
+      }, options);
+    });
+  },
 
+}
+
+function wrap(phantom) {
+  var wrapped = Object.create(phantom);
+  [ 'createPage',
+    'injectJs',
+    'addCookie',
+    'clearCookies',
+    'deleteCookie',
+    'set',
+    'get',
+    'exit'
+  ].forEach(function (method) {
+    var original = phantom[method];
+    wrapped[method] = function () {
+      var callback = arguments[arguments.length-1];
+      if (typeof callback === 'function') {
+        return original.apply(phantom, arguments);
+      } else {
+        return new Promise(function (resolve, reject) {
+          var args = Array.prototype.slice.call(arguments);
+          args.push(function (err, result) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(result);
+            }
+          });
+          original.apply(phantom, args);
+        });
+      }
+    }
+  });
 }
